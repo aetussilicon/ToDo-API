461baa34068a3a5a56fe25b7a3024e518aa8d9d7
Artur Gomes <arturgaollidev@outlook.com>
2025-02-12 18:35:06 -0300

    feat: configurações
    - Adicionados arquivos .yml com configurações do datasource como variáveis de ambiente e para o h2 para testes unitários.

--- src/test/java/br/teste/hit/todoapi/TodoapiApplicationTests.java
+++ src/test/java/br/teste/hit/todoapi/TodoapiApplicationTests.java
@@ -2,8 +2,10 @@ package br.teste.hit.todoapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TodoapiApplicationTests {
 
 	@Test
